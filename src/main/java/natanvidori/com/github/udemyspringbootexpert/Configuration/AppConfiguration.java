0ee251e79d45b60955246e66c4ac93c650f8c316
NatanVidori <natanvidorismo@hotmail.com>
2022-08-11 20:32:00 -0300

    Trabalhando com multiplos perfis de configurações no spring boot.

--- src/main/java/natanvidori/com/github/udemyspringbootexpert/Configuration/AppConfiguration.java
+++ src/main/java/natanvidori/com/github/udemyspringbootexpert/Configuration/AppConfiguration.java
@@ -1,13 +1,18 @@
 package natanvidori.com.github.udemyspringbootexpert.Configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 
 @Configuration
+@Profile("development")
 public class AppConfiguration {
     
-    @Bean(name = "applicationName")
-    public String applicationName() {
-        return "Sistema Spring Boot Expert";
+    @Bean
+    public CommandLineRunner executar() {
+        return args -> {
+            System.out.println("Rodando a configuração de desenvolvimento");
+        };
     }
 }
