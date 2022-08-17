1fa2335b7bfef1c507a23882af5504c49dcbbe11
NatanVidori <natanvidorismo@hotmail.com>
2022-08-17 19:06:25 -0300

    Melhorias na organização do projeto.

--- src/main/java/natanvidori/com/github/udemyspringbootexpert/Configuration/Animal.java
+++
@@ -1,5 +0,0 @@
-package natanvidori.com.github.udemyspringbootexpert.Configuration;
-
-public interface Animal {
-    void makeNoise();
-}
---
+++ src/main/java/natanvidori/com/github/udemyspringbootexpert/Configuration/Animal/Animal.java
@@ -0,0 +1,5 @@
+package natanvidori.com.github.udemyspringbootexpert.Configuration.Animal;
+
+public interface Animal {
+    void makeNoise();
+}
